c2f37897e5ca3f82b2c8182ed024809c9ac90a6a
heliacruz <82128352+heliacruz@users.noreply.github.com>
2021-04-18 14:06:35 -0500

    changes1

--- Patient_Info.py
+++ Patient_Info.py
@@ -2,6 +2,7 @@ age = 20
 first_name = "John"
 last_name = "Smith"
 is_new_patient = True
-print ("John")
-print ("Smith")
+#print ("John")
+#print ("Smith")
+print(first_name + " " + last_name)
 print (age)
