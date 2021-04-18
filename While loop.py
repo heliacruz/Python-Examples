b212e3f10503d268ec7699e4abe8a673b2dc47c9
heliacruz <82128352+heliacruz@users.noreply.github.com>
2021-04-18 14:32:06 -0500

    changes2

--- While loop.py
+++ While loop.py
@@ -1,9 +1,11 @@
 x = 0
-while x < 1:
+#while x < 1:
+while True:
 
-    if x ==5:
-        print('Found it!')
-        break
+    #if x ==5:
+        #print('Found it!')
+        #break
         #continue
     print (x)
     x += 1
+    
